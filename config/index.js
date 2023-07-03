c695ff9306c5717b057d53eb8959f78615e0ca14
lycheeh <138414414+lycheeh@users.noreply.github.com>
2023-07-03 09:34:36 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wxf59b939753834b1d",
+  appsecret: "4b8b4507f5140af6d81301f2281df633",
+  template_id: "QExT5MajS6Hv61ofaNQJoZiOKMQv52uyphs1pmkSunI",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "太原市",
+  first_date: "2023-7-3",
 };
